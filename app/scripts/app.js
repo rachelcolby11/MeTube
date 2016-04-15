f5a1386184b4f4ee7407a5c8e6a2efb7a8f70ee9
Rachel Colby <rachelcolby11@gmail.com>
2016-04-15 00:10:52 -0700

    Generate Angular app using Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name metubeApp
+ * @description
+ * # metubeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('metubeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
