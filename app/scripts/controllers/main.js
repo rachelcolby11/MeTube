4cf47602e2611b6ea6d97a00d5827bdd5273e853
Rachel Colby <rachelcolby11@gmail.com>
2016-04-15 01:43:59 -0700

    Display video titles as links to YouTube videos
    Currently displaying video titles which link to YouTube videos, with a placeholder thumbnail. Next, add thumbnails and link to embedded video player to be displayed when video is clicked on. Also, tiles holding video links need styling.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,39 @@
  */
 angular.module('metubeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    
+    $scope.showtunes = [
+      {
+        title: 'Satisfied',
+        musical: 'Hamilton',
+        url: 'http://www.youtube.com/watch?v=JrbCFR1FsZk' 
+      },
+      {
+        title: 'Not Getting Married Today',
+        musical: 'Company',
+        url: 'https://www.youtube.com/watch?v=3yW66pScmEs'
+      },
+      {
+        title: 'Finishing the Hat',
+        musical: 'Sunday in the Park with George',
+        url: 'https://www.youtube.com/watch?v=Sko2kjyrToc'
+      },
+      {
+        title: 'Being Alive',
+        musical: 'Company',
+        url: 'https://youtu.be/vC-6TYlMnlo?t=1m25s'
+      },
+      {
+        title: 'Jet Song',
+        musical: 'West Side Story',
+        url: 'https://www.youtube.com/watch?v=tlzdyUa-wx0'
+      },      
+      {
+        title: 'Wait for It',
+        musical: 'Hamilton',
+        url: 'https://www.youtube.com/watch?v=ReTP6x_sDiM'
+      }
     ];
   });
+
+       
